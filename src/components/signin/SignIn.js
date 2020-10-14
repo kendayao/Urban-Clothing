852fba447584e3533083fa8df7dd14b99f265e4e
Kaeneth Dayao <kendayao@gmail.com>
2020-10-14 08:43:43 -0700

    created signup with email and password feature

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import './SignIn.scss'
 import FormInput from '../form-input/FormInput'
 import CustomButton from '../custom-button/CustomButton'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends Component {
 
@@ -20,12 +20,18 @@ handleChange=(event)=>{
     this.setState({[name]:value})
 }
 
-handleSubmit=event=>{
+handleSubmit= async event=>{
     event.preventDefault();
 
-    this.setState({email:'', password:''})
-    
+    const {email, password } = this.state;
 
+    try{
+        await auth.signInWithEmailAndPassword(email, password);
+        this.setState({email:'', password:''})
+    }catch(error){
+        console.log(error)
+    }
+    
 }
 
 render(){
