d80deb58b68cc724f928849f49155b13f891aa90
Kaeneth Dayao <kendayao@gmail.com>
2020-10-14 22:16:37 -0700

    created cart icon component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
            
-          }, ()=>console.log(this.state))
+          })
         })
 
         
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
 import {auth} from '../../firebase/firebase.utils'
 import{ReactComponent as Logo} from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon'
 
 const Header = ({currentUser})=>{
     return(
@@ -19,6 +20,7 @@ const Header = ({currentUser})=>{
                 currentUser ? <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>:
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon/>
             </div>
             
         </div>
