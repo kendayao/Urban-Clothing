ae296fc6ab65a2d437a0366c419acd49d8f62b3f
Kaeneth Dayao <kendayao@gmail.com>
2020-12-11 12:40:59 -0800

    heroku

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
